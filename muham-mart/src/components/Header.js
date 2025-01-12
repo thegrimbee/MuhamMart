e70d3c096429b586e917eae6bb50ac99b328d32a
thegrimbee <gabriel.mario.antaputra@gmail.com>
2025-01-12 13:50:49 +0800

    Added header with muhammart text and placeholder profile

---
+++ muham-mart/src/components/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+
+const Header = () => {
+  return (
+    <>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <ShoppingCartIcon sx={{ mr: 2 }} />
+          <Typography variant="h6" color="inherit" noWrap>
+            MuhamMart
+          </Typography>
+          <Box sx={{ flexGrow: 1 }} />
+          <IconButton color="inherit">
+            <AccountCircle />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
+
+export default Header;
--- muham-mart/src/pages/Home.js
+++ muham-mart/src/pages/Home.js
@@ -7,22 +7,24 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import CssBaseline from '@mui/material/CssBaseline';
-import Grid from '@mui/material/Grid';
+import Grid2 from '@mui/material/Grid2';
 import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Link from '@mui/material/Link';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { ThemeProvider } from '@mui/material/styles';
 import theme from '../theme.js'
+import { useNavigate } from 'react-router-dom';
+import Header from '../components/Header.js';
 
 function Copyright() {
   return (
     <Typography variant="body2" color="text.secondary" align="center">
       {'Copyright © '}
       <Link color="inherit" href="https://mui.com/">
-        Muhammadiyah Welfare Home
+        MuhamMart
       </Link>{' '}
       {new Date().getFullYear()}
     </Typography>
@@ -47,93 +49,93 @@ const cards = [
   },
 ];
 
-export default function Album() {
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <AppBar position="relative">
-        <Toolbar>
-          <ShoppingCartIcon sx={{ mr: 2 }} />
-          <Typography variant="h6" color="inherit" noWrap>
-            MWH Minimart
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <main>
-        {/* Hero unit */}
-        <Box
-          sx={{
-            bgcolor: 'background.paper',
-            pt: 8,
-            pb: 6,
-          }}
-        >
-          <Container maxWidth="sm">
-            <Typography
-              component="h1"
-              variant="h2"
-              align="center"
-              color="text.primary"
-              gutterBottom
+const Home = () => {
+    const navigate = useNavigate();
+    const handleSignInClick = () => {
+        navigate('/login');
+    };
+
+    return (
+        <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Header></Header>
+        <main>
+            {/* Hero unit */}
+            <Box
+            sx={{
+                bgcolor: 'background.paper',
+                pt: 8,
+                pb: 6,
+            }}
             >
-              Welcome to MWH Minimart
-            </Typography>
-            <Typography variant="h5" align="center" color="text.secondary" paragraph>
-              Your one-stop shop for all your needs. Earn vouchers, shop products,
-              and track your orders - all in one place.
-            </Typography>
-            <Stack
-              sx={{ pt: 4 }}
-              direction="row"
-              spacing={2}
-              justifyContent="center"
-            >
-              <Button variant="contained">Sign In</Button>
-              <Button variant="outlined">Learn More</Button>
-            </Stack>
-          </Container>
-        </Box>
-        <Container sx={{ py: 8 }} maxWidth="md">
-          <Grid container spacing={4}>
-            {cards.map((card, index) => (
-              <Grid item key={index} xs={12} sm={6} md={4}>
-                <Card
-                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+            <Container maxWidth="sm">
+                <Typography
+                component="h1"
+                variant="h2"
+                align="center"
+                color="text.primary"
+                gutterBottom
                 >
-                  <CardMedia
-                    component="img"
-                    image={card.image}
-                    alt={card.title}
-                  />
-                  <CardContent sx={{ flexGrow: 1 }}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      {card.title}
-                    </Typography>
-                    <Typography>
-                      {card.description}
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </Grid>
-            ))}
-          </Grid>
-        </Container>
-      </main>
-      {/* Footer */}
-      <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
-        <Typography variant="h6" align="center" gutterBottom>
-          Muhammadiyah Welfare Home
-        </Typography>
-        <Typography
-          variant="subtitle1"
-          align="center"
-          color="text.secondary"
-          component="p"
-        >
-          Empowering residents through technology
-        </Typography>
-        <Copyright />
-      </Box>
-    </ThemeProvider>
-  );
+                Welcome to MuhamMart
+                </Typography>
+                <Typography variant="h5" align="center" color="text.secondary">
+                Your one-stop shop for all your needs. Earn vouchers, shop products,
+                and track your orders - all in one place.
+                </Typography>
+                <Stack
+                sx={{ pt: 4 }}
+                direction="row"
+                spacing={2}
+                justifyContent="center"
+                >
+                <Button variant="contained" onClick={handleSignInClick}>Sign In</Button>
+                <Button variant="outlined">Learn More</Button>
+                </Stack>
+            </Container>
+            </Box>
+            <Container sx={{ py: 8 }} maxWidth="md">
+            <Grid2 container spacing={4}>
+                {cards.map((card, index) => (
+                <Grid2 item key={index} size={{ xs: 12, sm: 6, md: 4}}>
+                    <Card
+                    sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                    >
+                    <CardMedia
+                        component="img"
+                        image={card.image}
+                        alt={card.title}
+                    />
+                    <CardContent sx={{ flexGrow: 1 }}>
+                        <Typography gutterBottom variant="h5" component="h2">
+                        {card.title}
+                        </Typography>
+                        <Typography>
+                        {card.description}
+                        </Typography>
+                    </CardContent>
+                    </Card>
+                </Grid2>
+                ))}
+            </Grid2>
+            </Container>
+        </main>
+        {/* Footer */}
+        <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
+            <Typography variant="h6" align="center" gutterBottom>
+            Muhammadiyah Welfare Home
+            </Typography>
+            <Typography
+            variant="subtitle1"
+            align="center"
+            color="text.secondary"
+            component="p"
+            >
+            Empowering residents through technology
+            </Typography>
+            <Copyright />
+        </Box>
+        </ThemeProvider>
+    );
 }
+
+export default Home;
