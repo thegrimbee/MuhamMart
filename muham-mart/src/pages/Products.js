e160f7f41ce071bc4c695619b3f5023bb66bed45
datletu <52278059+datletu@users.noreply.github.com>
2025-01-13 17:18:12 +0800

    add a bunch of stupid items

--- muham-mart/src/pages/Products.js
+++ muham-mart/src/pages/Products.js
@@ -1,38 +1,50 @@
 import * as React from 'react';
 import { Container, Grid2, Card, CardContent, CardMedia, Typography } from '@mui/material';
 import muhammadiyahLogo from '../assets/images/muhammadiyah_logo.png';
-
+import Daedalus from '../assets/images/Daedalus.jpg';
+import AghanimScepter from '../assets/images/Aghanim-Scepter.jpg';
+import Box from '@mui/material/Box';
 const products = [
-    { id: 1, name: 'Product 1', description: 'Description of Product 1', image: 'image1.jpg' },
-    { id: 2, name: 'Product 2', description: 'Description of Product 2', image: 'image2.jpg' },
+    { id: 1, name: 'Daedalus', description: '+69420 Damage', image: Daedalus },
+    { id: 2, name: 'Aghanim Scepter', description: '+1 Skill', image: AghanimScepter },
     // Add more products as needed
 ];
 
 export default function Products() {
     return (
-        <Container>
-            <Grid2 container spacing={4}>
-                {products.map((product) => (
-                    <Grid2 item key={product.id} xs={12} sm={6} md={4}>
-                        <Card>
-                            <CardMedia
-                                component="img"
-                                height="140"
-                                image={product.image}
-                                alt={product.name}
-                            />
-                            <CardContent>
-                                <Typography gutterBottom variant="h5" component="div" color="inherit">
-                                    {product.name}
-                                </Typography>
-                                <Typography variant="body2" color="inherit">
-                                    {product.description}
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                    </Grid2>
-                ))}
-            </Grid2>
-        </Container>
+        <>
+            <Box
+            sx={{
+                bgcolor: 'background.paper',
+                pt: 6,
+                pb: 0,
+            }}
+            ></Box>
+
+            <Container>
+                <Grid2 container spacing={4}>
+                    {products.map((product) => (
+                        <Grid2 item key={product.id} xs={12} sm={6} md={4}>
+                            <Card>
+                                <CardMedia
+                                    component="img"
+                                    image={product.image}
+                                    alt={product.name}
+                                />
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="div" color="inherit">
+                                        {product.name}
+                                    </Typography>
+                                    <Typography variant="body2" color="inherit">
+                                        {product.description}
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+                        </Grid2>
+                    ))}
+                </Grid2>
+            </Container>
+        </>
+        
     );
 }
