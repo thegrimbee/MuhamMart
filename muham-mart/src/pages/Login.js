d65e40d87f038bb8ed60afd874cbf1a6f459d0df
thegrimbee <gabriel.mario.antaputra@gmail.com>
2025-01-14 00:17:02 +0800

    Added firebase context and example of usage in login.js

---
+++ muham-mart/src/pages/Login.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import { useFirestore } from '../contexts/FirestoreContext';
+import { getDocs } from 'firebase/firestore';
+
+const Login = () => {
+  const { usersCollection } = useFirestore();
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const snapshot = await getDocs(usersCollection);
+      const usersList = snapshot.docs.map(doc => ({ ...doc.data(), id: doc.id }));
+      setUsers(usersList);
+    };
+
+    fetchUsers();
+  }, [usersCollection]);
+
+  return (
+    <div>
+      <h1>Current Users</h1>
+      <ul>
+        {users.map(user => (
+          <li key={user.Username}>{user.Username}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Login;
