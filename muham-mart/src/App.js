a483396ff10007d4e32026dc21f54d0f085a59f1
thegrimbee <gabriel.mario.antaputra@gmail.com>
2025-01-15 23:47:47 +0800

    made default page change depending on whether the user is logged in or not

--- muham-mart/src/App.js
+++ muham-mart/src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import Dashboard from './components/Dashboard';
 import About from './pages/About';
 import Products from './pages/Products';
 import Profile from './pages/Profile.js';
@@ -11,16 +10,17 @@ import ProductRequest from './pages/ProductRequest.js';
 import ProductDetails from './pages/ProductDetails';
 import theme from './theme.js';
 import { ThemeProvider } from '@emotion/react';
+import { useUser } from './contexts/UserContext.js';
 
 function App() {
+  const { user } = useUser();
   return (
     <ThemeProvider theme={theme}>
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={user ? <Navigate to="/products" /> : <Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/about" element={<About />} />
         <Route path="/products" element={<Products/>} />
         <Route path="/profile" element={<Profile />} />
